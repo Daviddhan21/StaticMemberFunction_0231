bf9c6347c4b66733cb031bf3b83628610992fa4e
Daviddhan21 <rayvanza.davida.ft24@mail.umy.ac.id>
2026-05-04 20:30:31 +0700

    isi constructor

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 class mahasiswa {
 public:
-     // constructor dengan parameter
+    mahasiswa(int nim, string nama); // constructor dengan parameter
 };
 
 // Definisi constructor dengan parameter
 mahasiswa::mahasiswa(int nim, string nama) {
     cout << "Constructor Dengan Parameter Terpanggil" << endl;
-    cout << "NIM : " << nim << endl;
+    
     cout << "Nama : " << nama << endl;
 }
 
